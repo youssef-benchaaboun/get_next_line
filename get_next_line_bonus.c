b00ab35f75381b1b1962ee82a06c86d8b6e89d58
youssef-benchaaboun <youssef.benchaaboun1@gmail.com>
2026-05-05 17:06:15 +0000

    day13

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -1,27 +1,27 @@
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 
 char	*get_next_line(int fd)
 {
-	static char	buffer[BUFFER_SIZE + 1];
+	static char	buffer[OPEN_MAX][BUFFER_SIZE + 1];
 	char		*line;
 	int			bytes;
 
 	line = NULL;
 	if (read(fd, NULL, 0) < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	if (buffer[0])
+	if (buffer[fd][0])
 		bytes = 1;
 	else
-		bytes = read(fd, buffer, BUFFER_SIZE);
+		bytes = read(fd, buffer[fd], BUFFER_SIZE);
 	while (bytes)
 	{
-		line = ft_strjoin(line, buffer);
-		ft_shift(buffer);
+		line = ft_strjoin(line, buffer[fd]);
+		ft_shift(buffer[fd]);
 		if (!line)
 			return (NULL);
 		if (line[ft_strlen(line)])
 			break ;
-		bytes = read(fd, buffer, BUFFER_SIZE);
+		bytes = read(fd, buffer[fd], BUFFER_SIZE);
 	}
 	if (bytes == -1)
 		return (free(line), (NULL));
--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -1,10 +1,14 @@
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 5
 # endif
+#ifndef OPEN_MAX
+# define OPEN_MAX 1024
+#endif
 # include <stdlib.h>
 # include <unistd.h>
+#include <limits.h>
 
 char	*get_next_line(int fd);
 int		ft_strlen(char *s);
